a2bab5bc9f519478a9b6a0186c1965210462185c
Bohorquez <jaelbkz@icloud.com>
2023-09-11 22:49:15 -0600

    error con el archivo persistence.xml y adicion de primer producto

--- src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
+++ src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
@@ -1,5 +1,25 @@
 package com.latam.alura.tienda.prueba;
 
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.latam.alura.tienda.modelo.Producto;
+
 public class RegistroDeProducto {
 
+    public static void main(String[] args) {
+        Producto sombrero = new Producto();
+        sombrero.setNombre("Sombrero Gucci ");
+        sombrero.setDescripcion("sombrero de alta gama con detalles en piel");
+        sombrero.setPrecio(new BigDecimal("18000.99"));
+
+        EntityManagerFactory eMgrFactory = Persistence.createEntityManagerFactory("tienda");
+        EntityManager eMgr = eMgrFactory.createEntityManager();
+
+        eMgr.persist(sombrero);
+
+    }
 }
